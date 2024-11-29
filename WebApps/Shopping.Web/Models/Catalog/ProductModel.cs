34d5be3aabdf2d2a28ea3b7df1871c1cfcf95591
Serhii Maslov <2021elit0049@ms.sumdu.edu.ua>
2024-11-29 20:19:17 +0200

    upd1

---
+++ WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Shopping.Web.Models.Catalog;
+
+public class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public decimal Price { get; set; }
+    public string ImageFile { get; set; } = default!;
+    public List<string> Category { get; set; } = new();
+}
+
+public record GerProductsResponse(IEnumerable<ProductModel> Products);
+public record GerProductsByCategoryResponse(IEnumerable<ProductModel> Products);
+public record GerProductsByIdResponse(ProductModel Products);
+
+
